c9bd092514b5fb993b7d8765de743759c597811f
M-Farag <mina.farag@icloud.com>
2023-04-19 17:49:08 +0200

    UPD The password generator part

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,15 @@
-fn main() {
-    println!("Hello, world!");
+use rand::{thread_rng, seq::SliceRandom};
+fn main(){ 
+
+    let length:usize = 10;
+    let chars_pool:Vec<char> = "abcdefghijklmnopqrstuvwxyz".chars().collect();
+
+    let password:String = (0..length).map(
+        |e|{
+            *chars_pool.choose(&mut rand::thread_rng()).unwrap()
+        }   
+     ).collect();
+
+     println!("Password: {}",password);
+
 }
