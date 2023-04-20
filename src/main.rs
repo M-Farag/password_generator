8f8db29cdabac3a47bc720c9ce8a51eb0fdd59b1
M-Farag <mina.farag@icloud.com>
2023-04-20 17:57:49 +0200

    ADD Struct opt crate
    UPD using struct opt crate to accept arguments and generate passwords

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,17 @@
 use rand::{thread_rng, seq::SliceRandom};
+use structopt::StructOpt;
+
+#[derive(Debug, StructOpt)]
+#[structopt(name="Password generator",about="My password generator")]
+struct Arguments {
+    #[structopt(long="length", short="l", default_value="8")]
+    length:usize
+}
 fn main(){ 
+
+    let arguments = Arguments::from_args();
+
+    println!("My arguments: {}",arguments.length);
 
     let length:usize = 10;
     let chars_pool:Vec<char> = "abcdefghijklmnopqrstuvwxyz".chars().collect();
